7090b765aa2a28d1ea20d97ca59c3420818995ed
SamanDev <mac.developers7@gmail.com>
2023-05-04 19:03:04 +0330

    s

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production"
-    ? undefined
+    ? "https://api.charkheshans.com/wheel"
     : "https://sock.charkheshans.com/wheel";
 
 const user = JSON.parse(localStorage.getItem("user"));
