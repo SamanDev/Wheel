e3c20985a2060f649f5b325cd8b19d3bca0168db
SamanDev <mac.developers7@gmail.com>
2023-05-11 07:34:12 +0330

    ss

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,27 +3,27 @@ import authHeader from "./auth-header";
 
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://api.charkheshans.com/api/test/"
-    : "http://localhost:8080/api/test/";
+    ? "https://api.charkheshans.com/"
+    : "http://localhost:8080/";
 const getPublicContent = () => {
-  return axios.get(API_URL + "all");
+  return axios.get(API_URL + "api/all");
 };
 
 const getUserBoard = () => {
-  return axios.get(API_URL + "user", { headers: authHeader() });
+  return axios.get(API_URL + "api/user", { headers: authHeader() });
 };
 const getchips = (id) => {
-  return axios.get("http://localhost:8080/getchip?id=" + id, {
+  return axios.get(API_URL + "getchip?id=" + id, {
     headers: authHeader(),
   });
 };
 
 const getModeratorBoard = () => {
-  return axios.get(API_URL + "mod", { headers: authHeader() });
+  return axios.get(API_URL + "api/mod", { headers: authHeader() });
 };
 
 const getAdminBoard = () => {
-  return axios.get(API_URL + "admin", { headers: authHeader() });
+  return axios.get(API_URL + "api/admin", { headers: authHeader() });
 };
 
 export default {
