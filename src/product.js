949e3908348a2d02a16565b1b0e94d21da3e8388
SamanDev <mac.developers7@gmail.com>
2023-05-26 23:12:57 +0330

    sxa

--- src/product.js
+++ src/product.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Card, Button } from "semantic-ui-react";
-
+import {
+  sumOfBet,
+  sumOfWin,
+  userBet,
+  segments,
+  getcolor,
+  getcolortext,
+  Jetton,
+} from "./utils/include";
 const CardExampleCardProps = (prop) => (
   <Card
     className="ltr"
@@ -9,12 +17,7 @@ const CardExampleCardProps = (prop) => (
     extra={
       <>
         <div style={{ float: "left" }}>
-          <lord-icon
-            src="https://cdn.lordicon.com/uvpkeeul.json"
-            trigger="morph"
-            colors="outline:#794628,primary:#e8b730,secondary:#e8b730"
-            style={{ width: 25, height: 25 }}
-          ></lord-icon>
+          <Jetton />
         </div>{" "}
         {prop.fee}{" "}
         <Button color="orange" size="mini" floated="right">
