deb650289b477525cf879888206005b27dd434c5
SamanDev <mac.developers7@gmail.com>
2023-06-02 03:11:45 +0330

    ss

--- src/adsComponent.js
+++ src/adsComponent.js
@@ -3,21 +3,22 @@ import React, { useEffect } from "react";
 const AdsComponent = (props) => {
   const { dataAdSlot } = props;
 
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {}
+  }, []);
+
   return (
     <>
-      <amp-ad
-        width="100vw"
-        height="320"
-        type="adsense"
-        className="adsbygoogle"
-        style={{ display: "block" }}
+      <ins
+        class="adsbygoogle"
+        style="display:block"
+        data-ad-format="fluid"
+        data-ad-layout-key="-6t+ed+2i-1n-4w"
         data-ad-client="ca-pub-8671523665871932"
-        data-ad-slot={dataAdSlot}
-        data-auto-format="rspv"
-        data-full-width=""
-      >
-        <div overflow=""></div>
-      </amp-ad>
+        data-ad-slot="5975694214"
+      ></ins>
     </>
   );
 };
