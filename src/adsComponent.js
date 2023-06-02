56299d33901829d4fe3d9946dce1dead7b5d8f6c
SamanDev <mac.developers7@gmail.com>
2023-06-02 03:41:44 +0330

    ddd

--- src/adsComponent.js
+++ src/adsComponent.js
@@ -3,21 +3,7 @@ import React, { useEffect } from "react";
 const AdsComponent = (props) => {
   const { dataAdSlot } = props;
 
-  return (
-    <>
-      <amp-ad
-        width="100vw"
-        height="320"
-        type="adsense"
-        data-ad-client="ca-pub-8671523665871932"
-        data-ad-slot="3569022726"
-        data-auto-format="rspv"
-        data-full-width=""
-      >
-        <div overflow=""></div>
-      </amp-ad>
-    </>
-  );
+  return <></>;
 };
 
 export default AdsComponent;
